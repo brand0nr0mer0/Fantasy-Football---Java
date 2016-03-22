a79ab12550508cc05d9b061f68d6f829c52d687b
Brandon Romero <brandon@beltranfamily.com>
2016-03-21 18:48:57 -0700

    Added jsoup and functions to get html

--- src/br/fantasy/webscripts/WebsiteAccessors.java
+++ src/br/fantasy/webscripts/WebsiteAccessors.java
@@ -1,7 +1,29 @@
 package br.fantasy.webscripts;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
-/**
- * Created by br9y2e2 on 3/21/16.
- */
+import java.io.IOException;
+
 public class WebsiteAccessors {
+
+
+    public Document getWebsiteResponse(String url) {
+        try {
+            Document doc = Jsoup.connect(url).get();
+            return doc;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public Elements getWebsiteUrls(Document doc) {
+        return doc.select("a[href]");
+    }
+
+
+    public Elements getWebsiteUrls(Elements elements) {
+        return elements.select("a[href]");
+    }
 }
